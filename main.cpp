7dd9726ec7810e1558b59eac63278d517e6c78af
Hatinggabi000 <kristinejoy.humilde@gmail.com>
2024-10-11 20:29:07 +0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -2,17 +2,24 @@
 
 using namespace std;
 
-int num, result, i;
+int num1;
+int num2;
+int sum;
+int numToSubtract;
+int difference;
 
 int main()
-
 {
-    cout << "Give any positive number: ";
-    cin >> num;
+    cout << "Enter the first number: ";
+    cin >> num1;
+    cout << "Enter the second number: ";
+    cin >> num2;
+    sum = num1 + num2;
+    cout << "Enter a number to subtract from the sum: ";
+    cin >> numToSubtract;
+    difference = sum - numToSubtract;
 
-     for (i = 1; i <= 10; i++) {
-     result = num * i;
-     cout << num << " * " << i << " = " << result << endl ;
-     }
-    return 0;
+    cout << "The sum of " <<num1<< " and " <<num2<< " is "<<sum<< ".\n";
+    cout << "The result after subtracting " <<numToSubtract<< " from " <<sum<< " is " <<difference<< ".";
+return 0;
 }
