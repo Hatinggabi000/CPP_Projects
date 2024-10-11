89ad0584e2a06152f9dffc1d2d905c9da9e3e629
Hatinggabi000 <kristinejoy.humilde@gmail.com>
2024-10-11 20:26:32 +0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -2,40 +2,17 @@
 
 using namespace std;
 
-float num1, num2, result;
-char operation;
+int num, result, i;
+
 int main()
+
 {
+    cout << "Give any positive number: ";
+    cin >> num;
 
-    cout << "Enter first number: ";
-    cin >> num1;
-    cout << "Enter second number: ";
-    cin >> num2;
-    cout << "Enter an operator: ";
-    cin >> operation;
-
-     switch (operation)
-    {
-        case '+' :
-            cout <<num1<< " + " <<num2<< " = " <<result<< "\n"<< endl;
-            break;
-        case '-':
-            cout <<num1<< " - " <<num2<< " = " <<result<< "\n"<< endl;
-            break;
-        case '*':
-            cout <<num1<< " * " <<num2<< " = " <<result<< "\n"<< endl;
-            break;
-        case '/':
-        if (num2 !=0){
-            result = num1 / num2;
-            cout << num1<< " / " <<num2<< " = " <<result<< "\n"<< endl;
-        }
-        else
-            cout << "Error! Divided by zero. \n";
-            break;
-        default:
-            cout << "Invalid operation" << endl;
-    }
-
+     for (i = 1; i <= 10; i++) {
+     result = num * i;
+     cout << num << " * " << i << " = " << result << endl ;
+     }
     return 0;
 }
