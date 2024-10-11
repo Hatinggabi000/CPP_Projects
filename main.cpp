fed34fe3596dcd6936671fea2a7a0512ce53ab3f
Hatinggabi000 <kristinejoy.humilde@gmail.com>
2024-10-11 20:10:03 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string name;
+    string address;
+    string school;
+    int age;
+    cout << "Please input your name: \n";
+    cin >> name;
+    cout << "Please input your address \n";
+    cin >> address;
+    cout << "Please input your school \n";
+    cin >> school;
+    cout << "please type your age";
+    cin >> age;
+
+    return 0;
+}
